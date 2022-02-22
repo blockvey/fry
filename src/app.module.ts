88fb1b78d55be735414319ba59250a6a65b7df2d
Hju Kneyck Flores <hjucode@gmail.com>
2022-02-22 08:41:21 +0800

    chore: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FormModule } from './form/form.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [FormModule],
+  imports: [FormModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
